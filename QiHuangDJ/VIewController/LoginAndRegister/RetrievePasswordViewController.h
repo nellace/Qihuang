084925de0cd103a5e881ec4d050f94c465ff54c8
Tzelann Minyi <qiann@live.cn>
2014-11-21 17:41:44 +0800

    「嘿嘿嘿嘿嘿嘿嘿嘿」

---
+++ QiHuangDJ/VIewController/LoginAndRegister/RetrievePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  RetrievePasswordViewController.h
+//  QiHuangDJ
+//
+//  Created by 朱子瀾 on 14-11-21.
+//  Copyright (c) 2014年 朱子瀾. All rights reserved.
+//
+
+#import "MQLCustomViewController.h"
+
+@interface RetrievePasswordViewController : MQLCustomViewController
+
+@end
---
+++ QiHuangDJ/VIewController/LoginAndRegister/RetrievePasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  RetrievePasswordViewController.m
+//  QiHuangDJ
+//
+//  Created by 朱子瀾 on 14-11-21.
+//  Copyright (c) 2014年 朱子瀾. All rights reserved.
+//
+
+#import "RetrievePasswordViewController.h"
+
+@interface RetrievePasswordViewController ()
+
+@end
+
+@implementation RetrievePasswordViewController
+
+
+
+#pragma mark - VIEW CONTROLLER LIFECYCLE
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self setCascTitle:@"找回密码"];
+        [self setFanhui];
+    }
+    
+    return self;
+}
+
+
+
+
+
+
+@end
