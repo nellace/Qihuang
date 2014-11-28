bba1196b5815430e75907a1b1cac372bf67cec20
Tzelann Minyi <qiann@live.cn>
2014-11-28 10:33:07 +0800

    papapa

--- QiHuangDJ/KHLDataManager/KHLUrls.h
+++ QiHuangDJ/KHLDataManager/KHLUrls.h
@@ -13,13 +13,13 @@
 #define KHLUrlBase @"http://www.175kh.com/api/"
 
 #pragma mark - 2.1 （2.1）注册新用户
-#define KHLUrlRegister @"app_register.php"//?username=%@&password=%@&sex=%@&email=%@"
+#define KHLUrlRegister @"app_register.php"
 
 #pragma mark - 2.2 （2.2）登录
-#define KHLUrlLogin @"app_login.php"//?loginname=%@&password=%@"
+#define KHLUrlLogin @"app_login.php"
 
 #pragma mark - 2.3 （2.22）找回密码
-#define KHLUrlRetrievePassword @"app_forget.php"//?email=%@"
+#define KHLUrlRetrievePassword @"app_forget.php"
 
 #pragma mark - 3.1 （2.3）获取推荐列表
 #define KHLUrlRecommendListAcquire @"app_tuijianlist.php"
@@ -33,7 +33,7 @@
 #pragma mark - 4.1 获取个人资料？
 
 #pragma mark - 4.2 （2.6）修改个人资料
-#define KHLUrlPersonalInformationModify @"app_editinfo.php"//?uid=%@&mobile=%@&nameture=%@&sex=%@&birthday=%@&blogaddress=%@&qq=%@&token=%@"
+#define KHLUrlPersonalInformationModify @"app_editinfo.php"
 
 #pragma mark - 5.1 （2.7）首页图片（轮播图、背景图）
 #define KHLUrlHomepageImagesAcquire @"app_home.php"
@@ -42,7 +42,7 @@
 #define KHLUrlLiveListAcquire @"app_zhibolist.php?game=%@"
 
 #pragma mark - 6.2 （2.9）获取直播详情（获取评论？）
-#define KHLUrlLiveDetailAcquire @"app_zhibo.php"//?info_id=%@&uid=%@&token=%@"
+#define KHLUrlLiveDetailAcquire @"app_zhibo.php"
 
 #pragma mark - 7.1 （2.14）获取点播列表
 #define KHLUrlVODListAcquire @"app_dianbolist.php?type=%@&cate_id=%@&p=%@&search=%@"
@@ -51,16 +51,16 @@
 #define KHLUrlVODDetailAcquire @"app_dianbo.php?info_id=%@&model=%@"
 
 #pragma mark - 8.1 （2.12）订阅
-#define KHLUrlSubscribeCategory @"app_dingyue.php"//?uid=%@&cate_id=%@&token=%@"
+#define KHLUrlSubscribeCategory @"app_dingyue.php"
 
 #pragma mark - 8.2 （2.13）退订
-#define KHLUrlUnsubscribeCategory @"app_quxiaodingyue.php"//?uid=%@&cate_id=%@&token=%@"
+#define KHLUrlUnsubscribeCategory @"app_quxiaodingyue.php"
 
 #pragma mark - 9.1 （2.16）添加收藏
-#define KHLUrlCollectArticle @"app_shoucang.php"//?uid=%@&info_id=%@&cate_id=%@&token=%@"
+#define KHLUrlCollectArticle @"app_shoucang.php"
 
 #pragma mark - 9.2 （2.17）取消收藏
-#define KHLUrlUncollectArticle @"app_quxiaodingyue.php"//?uid=%@&info_id=%@&cate_id=%@token=%@"
+#define KHLUrlUncollectArticle @"app_quxiaodingyue.php"
 
 #pragma mark - 10.1 （2.18）获取艺人列表
 #define KHLUrlEntertainersListAcquire @"app_ourstar.php"
@@ -75,10 +75,10 @@
 #define KHLUrlSearch @"app_search.php?search=%@&p=%@"
 
 #pragma mark - ?.? （2.11）举报
-#define KHLUrlReport @"app_jubao.php"//?info_id=%@&type=%@&uid=%@&token=%@&model=%@"
+#define KHLUrlReport @"app_jubao.php"
 
 #pragma mark - ?.? （2.10）评论
-#define KHLUrlCommentReply @"app_pinglun.php"//?uid=%@&info_id=%@&content=%@&token=%@&model=%@"
+#define KHLUrlCommentReply @"app_pinglun.php"
 
 #pragma mark - (2.23) 评论列表
 #define KHLUrlcommentlist @"app_commentlist.php"
